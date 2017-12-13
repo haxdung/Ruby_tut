0a87aa946f6cd0d13aacaebd0bde1d5c62240b2e
HaDung <dev.haxuandung97@gmail.com>
2017-12-13 09:15:21 +0700

    chap14

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,87 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+    :following, :followers]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :verify_admin, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    @microposts = @user.microposts.paginate(page: params[:page])
+    # redirect_to root_url unless @user
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = t "notification.check_email"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "notification.update_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = t "notification.login_please"
+        redirect_to login_url
+      end
+  end
+
+  def destroy
+    User.find_by(id: params[:id]).destroy
+    flash[:success] = t "notification.deleted"
+    redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit :name, :email, :password,
+        :password_confirmation
+    end
+
+    def correct_user
+      @user = User.find_by id: params[:id]
+      redirect_to root_url unless current_user? @user
+    end
+
+    def verify_admin
+      redirect_to(root_url) unless current_user.admin?
+    end
+end
---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+    config.load_defaults 5.1
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+  end
+end
