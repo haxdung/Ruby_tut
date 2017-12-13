d615c35a4f826464a265bad0c17cf7442e61c48d
HaDung <dev.haxuandung97@gmail.com>
2017-12-13 11:21:50 +0700

    chap10-ver3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+
+
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find_by id: params[:id]
   end
@@ -17,10 +27,47 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+  
+  def update
+    @user = User.find_by id: params[:id]
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    User.find_by(id:params[:id]).destroy
+    flash[:success] = t("user_deleted")
+    redirect_to users_url
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
         :password_confirmation)
     end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = t("log_in")
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find_by id: params[:id]
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+
   def log_in user
     session[:user_id] = user.id
   end
@@ -18,6 +19,10 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+  
+  def current_user? user 
+    user == current_user
+  end
 
   def current_user
     if (user_id = session[:user_id])
@@ -35,5 +40,15 @@ module SessionsHelper
   	forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+
+  def redirect_back_or default
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
