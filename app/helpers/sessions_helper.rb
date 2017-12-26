978eeadcc9dd456e2ead8d318a51656f3a496d28
HaDung <dev.haxuandung97@gmail.com>
2017-12-26 12:49:41 +0700

    chap11

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,17 +19,17 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
-  
+
   def current_user? user 
     user == current_user
   end
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      @current_user ||= User.find_by id: user_id
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -41,7 +41,6 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-
 
   def redirect_back_or default
     redirect_to(session[:forwarding_url] || default)
