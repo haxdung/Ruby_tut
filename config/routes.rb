015c2c89a117872a5a8867b74747889184c74184
HaDung <dev.haxuandung97@gmail.com>
2017-12-26 12:49:41 +0700

    chap7

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get  "users/new"
   root "static_pages#home"
   get  "/help",   to: "static_pages#help"
-  get  "/signup", to: "users#new" 
+  get  "/signup", to: "users#new"
+  resources :users 
 end
