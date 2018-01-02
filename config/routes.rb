10e64320fda2bdc10d9a0d4f6882bea73ac54c91
haxdung <dev.haxuandung97@gmail.com>
2018-01-02 21:58:14 +0700

    Revert "Chap7"

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get  "users/new"
   root "static_pages#home"
   get  "/help",   to: "static_pages#help"
-  get  "/signup", to: "users#new"
-  resources :users 
+  get  "/signup", to: "users#new" 
 end
